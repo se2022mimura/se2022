f525692d4703ec322e5c63504b678919237fb25e
r202103291as <r202103291as@jindai.jp>
2023-01-29 16:41:27 +0900

    using integer

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void){
-    printf("%d\n", 1);
-    printf("%d\n", 12);
-    printf("%d\n", 123);
-    printf("%d\n", 1234);
+    printf("%4d\n", 1);
+    printf("%4d\n", 12);
+    printf("%4d\n", 123);
+    printf("%4d\n", 1234);
     return 0;
 }
