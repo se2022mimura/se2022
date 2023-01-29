7400d0060aa2f8fcd07db29fe2545f49a0773efe
r202103291as <r202103291as@jindai.jp>
2023-01-29 16:39:55 +0900

    initial version

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(void){
-    int i = 10;
-    prontf("hello %d", i);
+    printf("%d\n", 1);
+    printf("%d\n", 12);
+    printf("%d\n", 123);
+    printf("%d\n", 1234);
     return 0;
 }
