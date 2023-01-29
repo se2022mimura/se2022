57fa7ac9f54b6858b4a2d32f026f4f9ecadd5805
r202103291as <r202103291as@jindai.jp>
2023-01-29 16:42:54 +0900

    print integer

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void){
-    printf("%4d\n", 1);
-    printf("%4d\n", 12);
-    printf("%4d\n", 123);
-    printf("%4d\n", 1234);
+    printf("%04d\n", 1);
+    printf("%04d\n", 12);
+    printf("%04d\n", 123);
+    printf("%04d\n", 1234);
     return 0;
 }
